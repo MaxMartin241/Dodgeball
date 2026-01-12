7ac9ef80592e9ade816a37353efc2303ae4f15a5
Max Martin <maxwmart244@ed.amdsb.ca>
2026-01-12 11:55:28 -0500

    I refurbished the movement system to be more physics based and added multi jumps.

--- Dodgeball/gameForm.cs
+++ Dodgeball/gameForm.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,49 +10,33 @@ namespace Dodgeball
         Rectangle bluePlayer = new Rectangle(670, 400, 35, 50);
         Rectangle redPlayer = new Rectangle(100, 400, 35, 50);
 
-        Rectangle redPlayerBounds = new Rectangle(400, 200, 27, 27);
-
         Rectangle middlebar = new Rectangle(408, 0, 10, 1000);
 
         SolidBrush blueBrush = new SolidBrush(Color.Blue);
         SolidBrush redBrush = new SolidBrush(Color.Red);
-
-        SolidBrush ballredBrush = new SolidBrush(Color.DarkRed);
-
         SolidBrush blackBrush = new SolidBrush(Color.Black);
 
-        Boolean wPressed = false;//red control buttons
+        bool wHeld = false;
+        bool upHeld = false;
 
-        Boolean sPressed = false;//red throw
+        bool aPressed, dPressed;
+        bool leftPressed, rightPressed;
 
-        Boolean aPressed = false;
-        Boolean dPressed = false;
+        int gravity = 2;
+        int jumpForce = -25;
 
-        Boolean upPressed = false;//blue control buttons
+        int redVelocityY = 0;
+        int blueVelocityY = 0;
 
-        Boolean downPressed = false;//blue throw
+        int groundY = 400;
 
-        Boolean leftPressed = false;
-        Boolean rightPressed = false;
-
-        int redStepSize = 5;
-        int blueStepSize = 5;
-
-        int[] redjumpSpeeds = new int[5] { -15, -11, -8, -5, -2 };
-        int[] bluejumpSpeeds = new int[5] { -15, -11, -8, -5, -2 };
-
-        bool redIsJumping = false;
-        bool redCanJump = true;
-
-        bool blueIsJumping = false;
-        bool blueCanJump = true;
-
-
+        int[] redStepSizes = new int[4] { 1, 2, 3, 4};
+        int[] blueStepSizes = new int[4] { 1, 2, 3, 4 };
 
         public gameForm()
         {
             InitializeComponent();
-            this.KeyPreview = true;
+            KeyPreview = true;
         }
 
         private void gameForm_Load(object sender, EventArgs e)
@@ -65,72 +44,31 @@ namespace Dodgeball
             gameTimer.Start();
         }
 
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            RedMove();
+            BlueMove();
+            ApplyRedGravity();
+            ApplyBlueGravity();
+            Invalidate();
+        }
+
         private void gameForm_Paint(object sender, PaintEventArgs e)
         {
             e.Graphics.FillRectangle(redBrush, redPlayer);
             e.Graphics.FillRectangle(blueBrush, bluePlayer);
-
             e.Graphics.FillRectangle(blackBrush, middlebar);
 
-            using (SolidBrush ballRedBrush = new SolidBrush(Color.Red))//calling dodgeball to make 2
+            using (SolidBrush ballBrush = new SolidBrush(Color.DarkRed))
             {
-                Dodgeball(e.Graphics, ballRedBrush, 100, 150);
-                Dodgeball(e.Graphics, ballRedBrush, 200, 150);
-
+                DrawBall(e.Graphics, ballBrush, 100, 150);
+                DrawBall(e.Graphics, ballBrush, 200, 150);
             }
-
-
-        }
-        void Dodgeball(Graphics g, SolidBrush ballRedBrush, int x, int y)//creating as meany balls as i want to put anywhere
-        {
-            int size = 27; // diameter of the ball
-
-            g.FillEllipse(ballRedBrush, x, y, size, size);//empty values for ball
         }
 
-
-
-        private void gameTimer_Tick(object sender, EventArgs e)
+        private void DrawBall(Graphics g, SolidBrush brush, int x, int y)
         {
-            redMove();
-            blueMove();
-            Invalidate();
-
-        }
-
-
-        private void redMove()
-        {
-            if (aPressed && redPlayer.X > 0)
-                redPlayer.X -= redStepSize;
-
-            if (dPressed && redPlayer.X < 406 - redPlayer.Width)
-                redPlayer.X += redStepSize;
-        }
-        private void blueMove()
-        {
-            //run into walls
-            if (leftPressed && bluePlayer.X > 415)
-                bluePlayer.X -= blueStepSize;
-
-            if (rightPressed && bluePlayer.X < Width - bluePlayer.Width - 20)
-                bluePlayer.X += blueStepSize;
-        }
-
-        private void gameForm_KeyUp(object sender, KeyEventArgs e)
-        {
-            switch (e.KeyCode)
-            {
-                case Keys.W: wPressed = false; break;
-                case Keys.S: sPressed = false; break;
-                case Keys.A: aPressed = false; break;
-                case Keys.D: dPressed = false; break;
-
-                case Keys.Up: upPressed = false; break;
-                case Keys.Down: downPressed = false; break;
-                case Keys.Left: leftPressed = false; break;
-                case Keys.Right: rightPressed = false; break;
-            }
+            g.FillEllipse(brush, x, y, 27, 27);
         }
 
         private void gameForm_KeyDown(object sender, KeyEventArgs e)
@@ -138,90 +76,130 @@ namespace Dodgeball
             switch (e.KeyCode)
             {
                 // RED
-                case Keys.W:
-                    if (redCanJump && !redIsJumping)
-                    {
-                        redCanJump = false;
-                        redIsJumping = true;
-                        _ = redJump(); // jump ONCE
-                    }
-                    break;
-
-                case Keys.S: sPressed = true; break;
                 case Keys.A: aPressed = true; break;
                 case Keys.D: dPressed = true; break;
 
-                // BLUE
-                case Keys.Up:
-                    if (blueCanJump && !blueIsJumping)
+                case Keys.W:
+                    if (!wHeld)
                     {
-                        blueCanJump = false;
-                        blueIsJumping = true;
-                        _ = blueJump(); // jump ONCE
+                        redVelocityY = jumpForce;
+                        wHeld = true;
                     }
                     break;
-                case Keys.Down: downPressed = true; break;
+
+                // BLUE
                 case Keys.Left: leftPressed = true; break;
                 case Keys.Right: rightPressed = true; break;
+
+                case Keys.Up:
+                    if (!upHeld)
+                    {
+                        blueVelocityY = jumpForce;
+                        upHeld = true;
+                    }
+                    break;
             }
         }
-        private async Task redJump()
+
+        private void gameForm_KeyUp(object sender, KeyEventArgs e)
         {
-            // jump up
-            for (int i = 0; i < redjumpSpeeds.Length; i++)
+            switch (e.KeyCode)
             {
-                redPlayer.Y += redjumpSpeeds[i];
-                await Task.Delay(15);
-            }
+                case Keys.A: aPressed = false; break;
+                case Keys.D: dPressed = false; break;
+                case Keys.W: wHeld = false; break;
 
-            // fall down
-            Array.Reverse(redjumpSpeeds);
-            for (int i = 0; i < redjumpSpeeds.Length; i++)
+                case Keys.Left: leftPressed = false; break;
+                case Keys.Right: rightPressed = false; break;
+                case Keys.Up: upHeld = false; break;
+            }
+        }
+
+        private async Task RedMove()
+        {
+            if (aPressed)
             {
-                redPlayer.Y -= redjumpSpeeds[i];
-                await Task.Delay(10);
+                for (int i = 0; i < redStepSizes.Length; i++)
+                {
+                    if (redPlayer.X <= 0)
+                    {
+                        redPlayer.X = 0;
+                        break;
+                    }
+                    redPlayer.X -= redStepSizes[i];
+                    await Task.Delay(5);
+                }
             }
-            Array.Reverse(redjumpSpeeds);
 
-            redIsJumping = false;
-            redCanJump = true;
-        }
-        private async Task blueJump()
-        {
-            // jump up
-            for (int i = 0; i < bluejumpSpeeds.Length; i++)
+            if (dPressed)
             {
-                bluePlayer.Y += bluejumpSpeeds[i];
-                await Task.Delay(15);
+                for (int i = 0; i < redStepSizes.Length; i++)
+                {
+                    if (redPlayer.X >= 410 - redPlayer.Width)
+                    {
+                        redPlayer.X = 410 - redPlayer.Width;
+                        break;
+                    }
+                    redPlayer.X += redStepSizes[i];
+                    await Task.Delay(5);
+                }
             }
+        }
 
-            // fall down
-            Array.Reverse(bluejumpSpeeds);
-            for (int i = 0; i < bluejumpSpeeds.Length; i++)
+        private async void BlueMove()
+        {
+            if (leftPressed)
             {
-                bluePlayer.Y -= bluejumpSpeeds[i];
-                await Task.Delay(10);
+                for (int i = 0; i < blueStepSizes.Length; i++)
+                {
+                    if (bluePlayer.X <= 417)
+                    {
+                        bluePlayer.X = 417;
+                        break;
+                    }
+                    bluePlayer.X -= blueStepSizes[i];
+                    await Task.Delay(5);
+                }
             }
-            Array.Reverse(bluejumpSpeeds);
 
-            blueIsJumping = false;
-            blueCanJump = true;
+            if (rightPressed)
+            {
+                for (int i = 0; i < blueStepSizes.Length; i++)
+                {
+                    if (bluePlayer.X >= 800 - bluePlayer.Width)
+                    {
+                        bluePlayer.X = 800 - bluePlayer.Width;
+                        break;
+                    }
+                    bluePlayer.X += blueStepSizes[i];
+                    await Task.Delay(5);
+                }
+            }
+        }
+        
+
+        private void ApplyRedGravity()
+        {
+            redVelocityY += gravity;
+            redPlayer.Y += redVelocityY;
+
+            if (redPlayer.Y >= groundY)
+            {
+                redPlayer.Y = groundY;
+                redVelocityY = 0;
+            }
         }
 
-
-
-        private void redThrow()
+        private void ApplyBlueGravity()
         {
+            blueVelocityY += gravity;
+            bluePlayer.Y += blueVelocityY;
 
-        }
-        private void blueThrow()
-        {
-
-        }
-
-        private void gameForm_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            
+            if (bluePlayer.Y >= groundY)
+            {
+                bluePlayer.Y = groundY;
+                blueVelocityY = 0;
+            }
         }
     }
 }
